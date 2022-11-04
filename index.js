d01eb1746cebd659cc7c7055bce228cc593a4183
GlassPainter <ckwong6599@gmail.com>
2022-11-04 13:55:44 -0400

    11/04/2022
    Started

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
-  box: document.getElementById("container-box"),
-  input: document.querySelector(`#input`),
+  Box: document.getElementById("Box"),
+  Text: document.querySelector("Box-Text"),
+  Button: document.querySelector("Button"),
 };
-/* 
-const cat = "meow";
-DOMSelectors.box.insertAdjacentHTML("afterbegin", `<h1>We are a ${cat}</h1>`);
- */
 
-DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
-  DOMSelectors.input.value = "";
+DOMSelectors.Button.addEventListener("click", function () {
+  let Input = DOMSelectors.Text.value;
+  DOMSelectors.Box.insertAdjacentHTML("beforeend", `<p>${Input}</p>`);
 });
