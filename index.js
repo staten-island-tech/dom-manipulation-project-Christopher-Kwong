c243004d955dda8c0aaf70586bf942e7cff8e70f
Christopher Kwong <ckwong6599@gmail.com>
2022-11-06 16:49:08 -0500

    11/06/2022
    Need 1 & 4 Function

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const DOMSelectors = {
-  Box: document.getElementById("Box"),
-  Text: document.querySelector("Box-Text"),
-  Button: document.querySelector("Button"),
+  Input: document.querySelector("Input"),
+  Create: document.getElementById("Create"),
+  Inject: document.getElementById("Inject"),
+  Clear: document.getElementById("Clear"),
+  Remove: document.getElementById("Remove"),
+
+  Output_Box: document.getElementById("Output-Box"),
 };
 
-DOMSelectors.Button.addEventListener("click", function () {
-  let Input = DOMSelectors.Text.value;
-  DOMSelectors.Box.insertAdjacentHTML("beforeend", `<p>${Input}</p>`);
+/* DOMSelectors.Button.addEventListener("click", function () {
+  let input = DOMSelectors.Input.value;
+  DOMSelectors.Box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.Input.value = "";
+});
+ */
+
+/* DOMSelectors.Create.addEventListener("click", function () {});
+ */
+DOMSelectors.Inject.addEventListener("click", function () {
+  let input = DOMSelectors.Input.value;
+  DOMSelectors.Output_Box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+});
+
+DOMSelectors.Clear.addEventListener("click", function () {
+  DOMSelectors.Input.value = "";
+});
+
+DOMSelectors.Remove.addEventListener("click", function () {
+  DOMSelectors.Input.remove();
 });
