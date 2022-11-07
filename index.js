92f48e0151472d727d27751dba9b5bea9449f639
GlassPainter <ckwong6599@gmail.com>
2022-11-07 13:56:45 -0500

    11/07/2022
    Wrong thing

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const DOMSelectors = {
+/* const DOMSelectors = {
   Input: document.querySelector("Input"),
   Create: document.getElementById("Create"),
   Inject: document.getElementById("Inject"),
   Clear: document.getElementById("Clear"),
   Remove: document.getElementById("Remove"),
-  Clean: document.getElementById("Clean"),
 
   Output_Box: document.getElementById("Output-Box"),
 };
@@ -27,9 +26,32 @@ DOMSelectors.Clear.addEventListener("click", function () {
 });
 
 DOMSelectors.Remove.addEventListener("click", function () {
-  CreatedObject = "";
+  DOMSelectors.Output_Box.innerHTML = "";
 });
 
-DOMSelectors.Clean.addEventListener("click", function () {
-  DOMSelectors.Output_Box.innerHTML = "";
+ */
+
+const DOMSelectors = {
+  Input: document.querySelector("Input"),
+  Create: document.getElementById("Create"),
+  Inject: document.getElementById("Inject"),
+  Clear: document.getElementById("Clear"),
+  Remove: document.getElementById("Remove"),
+
+  Output_Box: document.getElementById("Output-Box"),
+};
+
+let Input = "";
+
+DOMSelectors.Create.addEventListener("click", function () {
+  Input = DOMSelectors.Input.value;
+  DOMSelectors.Output_Box.insertAdjacentHTML("beforeend", `<p>${Input}</p>`);
+});
+
+DOMSelectors.Clear.addEventListener("click", function () {
+  DOMSelectors.Input.value = "";
+});
+
+DOMSelectors.Remove.addEventListener("click", function () {
+  Input.innerHTML = "";
 });
