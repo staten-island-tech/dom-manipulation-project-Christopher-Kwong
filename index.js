daecafa90dfbe35a9b467052236345ac75aab685
GlassPainter <ckwong6599@gmail.com>
2022-11-16 13:40:57 -0500

    11/16/2022
    Issue testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* const DOMSelectors = {
+const DOMSelectors = {
   Form: document.getElementById("Form"),
   Create: document.getElementById("Create"),
   Image: document.getElementById("Input"),
@@ -10,47 +10,11 @@ function CreateObject(inputString) {
   return `<div class = Box>
       <img width="200px" src ="${inputString}">
       <br>
-      <button type ="click" id = "Remove" onClick="RemoveObject();">Remove</button>
-    </div>`;
-}
-
-function InsertObject(newObject, atHere) {
-  atHere.insertAdjacentHTML("beforeend", newObject);
-}
-
-function Clear(inputField) {
-  inputField.value = "";
-}
-
-function RemoveObject() {
-  DOMSelectors.Output.innerHTML = "";
-}
-
-DOMSelectors.Form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let url = DOMSelectors.Image.value;
-  InsertObject(CreateObject(url), DOMSelectors.Output);
-  Clear(DOMSelectors.Image);
-});
- */
-
-/* const DOMSelectors = {
-  Form: document.getElementById("Form"),
-  Create: document.getElementById("Create"),
-  Image: document.getElementById("Input"),
-
-  Output: document.getElementById("Output"),
-};
-
-function CreateObject(inputString) {
-  return `<div id = Box>
-      <img width="200px" src ="${inputString}">
-      <br>
       <button type ="click" id = "Remove">Remove</button>
     </div>`;
 }
 
-function InsertObject(atHere, newObject) {
+function InsertObject(newObject, atHere) {
   atHere.insertAdjacentHTML("beforeend", newObject);
 }
 
@@ -67,32 +31,15 @@ function Repeat() {
 
 DOMSelectors.Form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   let url = DOMSelectors.Image.value;
-  InsertObject(DOMSelectors.Output, CreateObject(url));
+  InsertObject(CreateObject(url), DOMSelectors.Output);
   Clear(DOMSelectors.Image);
   Repeat();
-}); */
+});
 
-/* let i = 0;
-let Text = "";
+Repeat();
 
-for (; i < 10; i++) {
-  Text += `<button id = "Remove${i}">Remove${i}</button>`;
-}
-
-document.getElementById("demo").innerHTML = Text;
-
-function Repeat() {
-  let body = document.getElementById("Body");
-  Remove1.addEventListener("click", function () {
-    body.innerHTML = "<p></p>";
-  });
-}
-
-Repeat(); */
-
-let counter = 0;
+/* let counter = 0;
 let Text = "";
 
 for (; counter <= 10; counter++) {
@@ -112,3 +59,9 @@ function removeElement(Id) {
 }
 
 removeElement(counter);
+  let Remove = document.getElementById("Remove");
+  Remove.addEventListener("click", function () {
+    DOMSelectors.Output.innerHTML = "";
+  });
+});
+ */
