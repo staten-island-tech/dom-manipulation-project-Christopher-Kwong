392a20eea3240845a88ac86e14440b5281cea568
GlassPainter <ckwong6599@gmail.com>
2022-11-18 10:58:11 -0500

    11/18/2022
    Trying to implement color change, not working

--- Working/index.js
+++ Working/index.js
@@ -4,11 +4,13 @@ const DOMSelectors = {
   Image: document.getElementById("ImageInput"),
   Text: document.getElementById("TextInput"),
 
+  Blue: document.getElementById("Color-Blue"), //Testing
+
   Output: document.getElementById("Output"),
 };
 
 function CreateObject(Input, SecondInput) {
-  return `<div>
+  return `<div class="Box"> 
     <img class="Image" height="80px" src ="${Input}">
     <p class="Text">${SecondInput}</p>
   </div>`;
@@ -30,12 +32,20 @@ function Remove() {
   });
 }
 
+function BlueChange() {
+  //Testing
+  DOMSelectors.Blue.addEventListener("click", function () {
+    DOMSelectors.Text.color = "blue";
+  });
+}
+
 DOMSelectors.Form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let url = DOMSelectors.Image.value;
   let text = DOMSelectors.Text.value;
   InsertObject(DOMSelectors.Output, CreateObject(url, text));
+  BlueChange(); //Testing
   Clear(DOMSelectors.Image, DOMSelectors.Text);
   Remove();
 });
