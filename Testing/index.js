c0696740e2e8e4e6fda656dc0d9349aefd783e6f
Christopher Kwong <ckwong6599@gmail.com>
2022-11-17 22:20:26 -0500

    11/17/2022

--- Testing/index.js
+++ Testing/index.js
@@ -1,41 +1,5 @@
-/* const DOMSelectors = {
-  Form: document.getElementById("Form"),
-  Create: document.getElementById("Create"),
-  Image: document.getElementById("Input"),
-
-  Output: document.getElementById("Output"),
-};
-
-function CreateObject(inputString) {
-  return `<div class = Box>
-      <img width="200px" src ="${inputString}">
-      <br>
-      <button type ="click" id = "Remove" onClick="RemoveObject();">Remove</button>
-    </div>`;
-}
-
-function InsertObject(newObject, atHere) {
-  atHere.insertAdjacentHTML("beforeend", newObject);
-}
-
-function Clear(inputField) {
-  inputField.value = "";
-}
-
-function RemoveObject() {
-  DOMSelectors.Output.innerHTML = "";
-}
-
-DOMSelectors.Form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let url = DOMSelectors.Image.value;
-  InsertObject(CreateObject(url), DOMSelectors.Output);
-  Clear(DOMSelectors.Image);
-});
- */
-
 const DOMSelectors = {
-  Form: document.getElementById("Form"),
+  Form: document.getElementById("MyForm"),
   Create: document.getElementById("Create"),
   Image: document.getElementById("Input"),
 
@@ -45,46 +9,36 @@ const DOMSelectors = {
 let createObjectCounter = 1; // keep track how many new objects have been created and use its value to form a unique element id for the new object.
 
 function CreateObject(inputString, objectID, buttonID) {
-  // return '<div id="ImgBox-1"><img width="100px" src="https://www.massmutual.com/global/media/shared/img/people-on-steps.jpg"><br><button type="click" id="ImgButton-1">Remove</button></div>';
-  let str1 = '<div id="' + objectID;
-  let str2 = str1 + '"><img width="100px" src="' + inputString;
-  let str3 =
-    // str2 + '"><br><button type="click" id="ImgButton-0">Remove</button></div>';
-    str2 +
-    '"><br><button type="click" id="' +
+  // https://www.javascripttutorial.net/dom/manipulating/create-a-dom-element/
+  //https://www.w3schools.com/js/js_htmldom.asp
+
+  return (
+    '<div id="' +
+    objectID +
+    '"><img width="100px" src="' +
+    inputString +
+    '"><br><button type="button" id="' +
     buttonID +
-    '">Remove</button></div>';
-  // let str3 = str2 + '"><br><button type="click" id="' + buttonID;
-  // let str4 = str3 + '">Remove</button></div>';
-  return str3;
-  // return (
-  //   '<div id="' +
-  //   objectID +
-  //   '"><img width="100px" src="' +
-  //   inputString +
-  //   '"><br><button type="click" id="' +
-  //   buttonID +
-  //   '">Remove</button></div>'
-  // );
+    '" onclick=RemoveObject("' +
+    objectID +
+    '")>Remove</button></div>'
+  );
 }
 
 function InsertObject(atHere, newObject) {
   console.log("InsertObject: \n" + newObject);
-  // DOMSelectors.Output.insertAdjacentHTML("beforeend", newObject);
-  // atHere.insertAdjacentHTML(
-  DOMSelectors.Output.insertAdjacentHTML("beforeend", "" + newObject);
-  //   `<div id = "ImgBox0">
-  //       <img width="500px" src ="https://th.bing.com/th/id/OIP.aa4s3_RarNq7iKSI084hswHaJ4?w=146&h=194&c=7&r=0&o=5&pid=1.7">
-  //       <br>
-  //       <button type ="click" id = "ImgButton0">Remove</button>
-  //     </div>`
-  // );
+  // DOMSelectors.Output.insertAdjacentHTML("beforeend", "" + newObject);
+  atHere.insertAdjacentHTML("beforeend", "" + newObject);
 }
 
-function RemoveObject(event) {
-  console.log("-------------------- Remove Object id:");
-  // document.getElementById(objectID).innerHTML = "<p></p>";
-  event.currentTarget.parentElement.innerHTML = "";
+function RemoveObject(objectID) {
+  console.log("-------------------- objectId:" + objectID);
+  console.log(
+    "-------------------- to be Removed Object id:" +
+      document.getElementById(objectID).id
+  );
+  document.getElementById(objectID).innerHTML = "<p></p>";
+  // event.currentTarget.parentElement.innerHTML = "";
 }
 
 function Clear(inputField) {
@@ -100,7 +54,7 @@ function Clear(inputField) {
 
 DOMSelectors.Form.addEventListener("submit", (e) => {
   e.preventDefault();
-  e.stopPropagation();
+  // e.stopPropagation();
 
   let url = DOMSelectors.Image.value;
   let newObjectID = "ImgBox-" + createObjectCounter;
@@ -112,48 +66,8 @@ DOMSelectors.Form.addEventListener("submit", (e) => {
     CreateObject(url, newObjectID, newButtonID)
   );
   let newButton = document.getElementById(newButtonID);
-  console.log("=========================");
-  window.alert("going to add listener");
-  newButton.addEventListener("click", RemoveObject(e));
+  console.log("=========================" + newButton.id);
+  // newButton.onclick = RemoveObject(newObjectID);
 
   Clear(DOMSelectors.Image);
 });
-
-/* let i = 0;
-let Text = "";
-
-for (; i < 10; i++) {
-  Text += `<button id = "Remove${i}">Remove${i}</button>`;
-}
-
-document.getElementById("demo").innerHTML = Text;
-
-function Repeat() {
-  let body = document.getElementById("Body");
-  Remove1.addEventListener("click", function () {
-    body.innerHTML = "<p></p>";
-  });
-}
-
-Repeat(); */
-
-// let counter = 0;
-// let Text = "";
-
-// for (; counter <= 10; counter++) {
-//   Text += `<div id=Body>
-//     <button onClick="removeElement">Remove${counter}</button>
-//   </div>`;
-// }
-
-// document.getElementById("demo").innerHTML = Text;
-
-// function removeElement(Id) {
-//   let Body = `Body + ${Id}`;
-//   let Remove = `Remove + ${Id}`;
-//   Remove.addEventListener("click", function () {
-//     Body.innerHTML = "<p></p>";
-//   });
-// }
-
-// removeElement(counter);
--- Working/index.js
+++ Working/index.js
@@ -1,68 +1,26 @@
 const DOMSelectors = {
   Form: document.getElementById("Form"),
   Create: document.getElementById("Create"),
-  Image: document.getElementById("Input"),
+  Image: document.getElementById("ImageInput"),
+  Text: document.getElementById("TextInput"),
 
   Output: document.getElementById("Output"),
 };
 
-function CreateObject(Input) {
-  return `<img class="Image" height="80px" src ="${Input}">`;
-}
-
-function InsertObject(AtHere, NewObject) {
-  AtHere.insertAdjacentHTML("beforeend", NewObject);
-}
-
-function Clear(inputField) {
-  inputField.value = "";
-}
-
-function Remove() {
-  let RemoveBox = document.getElementById("Remove");
-  RemoveBox.addEventListener("click", function () {
-    DOMSelectors.Output.innerHTML = "";
-  });
-}
-
-DOMSelectors.Form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let url = DOMSelectors.Image.value;
-  InsertObject(DOMSelectors.Output, CreateObject(url));
-  Clear(DOMSelectors.Image);
-  Remove();
-});
-
-/* let Counter = 1;
-
-const DOMSelectors = {
-  Form: document.getElementById("Form"),
-  Create: document.getElementById("Create"),
-  Image: document.getElementById("Input"),
-
-  LinkArea: document.getElementById("LinkArea"),
-  Output: document.getElementById("Output"),
-};
-
-function CreateUrl() {
-  Counter = Counter + 1;
+function CreateObject(Input, SecondInput) {
   return `<div>
-    <label for="Input${Counter}">Image ${Counter}:</label>
-    <input id="Input${Counter}" Input="Input" type="text"/>
+    <img class="Image" height="80px" src ="${Input}">
+    <p class="Text">${SecondInput}</p>
   </div>`;
 }
 
-function CreateObject(Input) {
-  return `<img class="Image" width="200px" src ="${Input}${Counter}">`;
-}
-
 function InsertObject(AtHere, NewObject) {
   AtHere.insertAdjacentHTML("beforeend", NewObject);
 }
 
-function Clear(inputField) {
-  inputField.value = "";
+function Clear(Input, Input2) {
+  Input.value = "";
+  Input2.value = "";
 }
 
 function Remove() {
@@ -76,8 +34,8 @@ DOMSelectors.Form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let url = DOMSelectors.Image.value;
-  InsertObject(DOMSelectors.Output, CreateObject(url));
-  InsertObject(DOMSelectors.LinkArea, CreateUrl());
-  Clear(DOMSelectors.Image);
+  let text = DOMSelectors.Text.value;
+  InsertObject(DOMSelectors.Output, CreateObject(url, text));
+  Clear(DOMSelectors.Image, DOMSelectors.Text);
   Remove();
-}); */
+});
